8cf7873209d24235c9d5892ea1c1288335fcbbee
Fjeldhammer <fjeld.uilleam@gmail.com>
2023-07-26 14:13:28 -0600

    added  new code

--- js/site.js
+++ js/site.js
@@ -1,14 +1,21 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
+const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOGJiOWUzYTc3OTQxMmJlNjlkNzBlNzZjOGQ3ZjgxNCIsInN1YiI6IjY0YzE2YTQ1ZGY4NmE4MDEyNTgyODkzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MO5hOqxWBxw_oWz9eMDX58JLcsBwbV-eZLk6dnDqPS8'
 
-    let message = inputBox.value;
+// https://api.themoviedb.org/3/movie/popular
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+async function getMovies() {
+
+    try {
+
+        let response = await fetch('https://api.themoviedb.org/3/movie/popular', {
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`
+            }
+        });
+
+        let data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
 }
-
